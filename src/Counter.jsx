0521f8a8396e6126edfa1182cc1bb883be7808d3
Andie Kardos <andreja.kardos@conserveitiot.com>
2021-04-13 13:24:10 +1000

    Added useReducer hook to Counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,21 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 }
+        case 'decrement':
+            return { count: state.count - 1}
+        default: 
+            return state
+    }
+}
 
 export default function Counter() {
-    const [count, setCount] = useState(0)
+    const [state, dispatch] = useReducer(reducer, { count: 0 })
 
     function increase() {
-        setCount(prevCount => prevCount + 1)
+        dispatch({ type: 'increment' })
     }
 
     function decrease() {
-        setCount(prevCount => prevCount - 1)
+        dispatch({ type: 'decrement' })
     }
 
     return (
         <>
         <div></div>
             <button onClick={decrease}>-</button>
-            <span>{count}</span>
+            <span>{state.count}</span>
             <button onClick={increase}>+</button>
         </>
     )
