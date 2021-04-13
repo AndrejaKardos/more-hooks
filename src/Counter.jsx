be8a741cd2c4547ebcd8e8bdf58ee9babf8f0ddc
Andie Kardos <andreja.kardos@conserveitiot.com>
2021-04-13 13:27:02 +1000

    Added ACTIONS constants to replace strings

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useReducer } from 'react'
 
+const ACTIONS = {
+    INCREMENT: 'increment',
+    DECREMENT: 'decrement'
+}
+
 function reducer(state, action) {
     switch (action.type) {
-        case 'increment':
+        case ACTIONS.INCREMENT:
             return { count: state.count + 1 }
-        case 'decrement':
+        case ACTIONS.DECREMENT:
             return { count: state.count - 1}
         default: 
             return state
@@ -15,11 +20,11 @@ export default function Counter() {
     const [state, dispatch] = useReducer(reducer, { count: 0 })
 
     function increase() {
-        dispatch({ type: 'increment' })
+        dispatch({ type: ACTIONS.INCREMENT })
     }
 
     function decrease() {
-        dispatch({ type: 'decrement' })
+        dispatch({ type: ACTIONS.DECREMENT })
     }
 
     return (
